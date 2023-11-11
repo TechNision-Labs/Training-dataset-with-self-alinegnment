76d60d1d1fb1598908d3a69c2c8ec07d113a88ab
Eber <laurente.eber@outlook.com>
2023-11-11 17:10:31 -0500

    Prototyping prompts with langchain

--- bot/ia_instructions.py
+++ bot/ia_instructions.py
@@ -1,11 +1,13 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+
 from rich.console import Console
 from utils import (DocsJSONLLoader, get_file_path, get_openai_api_key)
 
 console = Console()
-recreate_chroma_db=True
+recreate_chroma_db=False
 
 def load_documents(file_path: str):
     loader = DocsJSONLLoader(file_path)
@@ -26,7 +28,11 @@ def get_chroma_db(embeddings, documents, path):
     else:
         console.print("CARGANDO CHROMA EXISTENTE")
         return Chroma(persist_directory=path, embedding_function=embeddings)
-    
+
+
+def extraction_instruction(vectorstore, llm):
+    print(vectorstore.schema)
+
 
 def main():
     documents = load_documents(get_file_path())
@@ -36,8 +42,15 @@ def main():
 
     vectorstore_chroma = get_chroma_db(embeddings, documents, "chroma_docs")
     console.print(f"[green]Documentos {len(documents)} cargando.[/green]")
-    print(vectorstore_chroma)
+    #print(vectorstore_chroma)
 
+    llm = ChatOpenAI(
+      model_name="gpt-3.5-turbo",
+      temperature=0.2,
+      max_tokens=1000,  # Queremos que las respuestas sean tan largas como el modelo lo considere, sin considerar la cantidad de tokens
+    )
+
+    extraction_instruction(vectorstore_chroma, llm)
 
 if __name__ == "__main__":
     main()
